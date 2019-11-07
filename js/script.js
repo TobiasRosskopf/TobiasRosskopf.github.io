86346cb4b0268630aa5d5e36a795dc6cedbd0dbb
Tobias Rosskopf (privat) <tobirosskopf@gmail.com>
2019-11-07 21:39:06 +0100

    Changed events

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-var events = require('./events.json');
+const eventsData = [
+    {
+        date: "2019/02/??",
+        event: "DAWAKO",
+        location: "TU Darmstadt"
+    },
+    {
+        date: "2018/11/??",
+        event: "Seminar Starkregen",
+        location: "TU München"
+    }
+];
+
+function showEvents() {
+    var data_row =  "";
+    for (let i = 0; i < eventsData.length; i++) {
+        const event = eventsData[i];
+        data_row += "<tr><td>" + event.date + "</td><td>" + event.event + "</td><td>" + event.location + "</td></tr>";
+    };
+    document.getElementById("events-data").innerHTML = data_row;
+}
