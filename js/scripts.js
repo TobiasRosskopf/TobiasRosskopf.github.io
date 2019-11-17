7807a35faf8fa887306bf8b5e82f790a6ec735e2
Tobias Rosskopf <tobias.rosskopf@icloud.com>
2019-11-17 11:31:31 +0100

    Changed skills

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ function showSkills() {
 var data_row =  "";
     for (let i = 0; i < skillsData.length; i++) {
         const skill = skillsData[i];
-        data_row += "<tr><td>" + skill.skill + "</td><td><a href='" + skill.link + "'>" + skill.started + "</a></td><td>" + skill.level + "</td></tr>";
+        data_row += "<tr><td><a href='" + skill.link + "'>" + skill.skill + "</a></td><td>" + skill.category + "</td><td>" + skill.started + "</td><td>" + skill.level + " %</td></tr>";
     };
     document.getElementById("skills-data").innerHTML = data_row;
 }
--- js/skillsData.js
+++ js/skillsData.js
@@ -3,6 +3,21 @@ const skillsData = [
         skill: "Python",
         started: 2017,
         level: 80,
+        category: "Programming Language",
         link: "https://www.python.org"
-    }
+    },
+   {
+        skill: "Fortran",
+        started: 2016,
+        level: 30,
+        category: "Programming Language",
+        link: "https://www.fortran.com"
+    },
+   {
+        skill: "Java",
+        started: 2009,
+        level: 65,
+        category: "Programming Language",
+        link: "https://www.java.com"
+    },
 ];
