d9ca0ff2f0bec0628a658cf95d38632bad34a3ec
Tobias Rosskopf (privat) <tobirosskopf@gmail.com>
2019-11-18 13:10:03 +0100

    Changed skills

--- js/skillsData.js
+++ js/skillsData.js
@@ -4,16 +4,8 @@ const skillsData = [
         description: "",
         category: "Programming Language",
         started: 2017,
-        level: 80,
+        level: 90,
         link: "https://www.python.org"
-    },
-   {
-        skill: "Fortran",
-        description: "",
-        category: "Programming Language",
-        started: 2016,
-        level: 30,
-        link: "https://www.fortran.com"
     },
    {
         skill: "Java",
@@ -24,6 +16,70 @@ const skillsData = [
         link: "https://www.java.com"
     },
     {
+        skill: "C#",
+        description: "",
+        category: "Programming Language",
+        started: 2014,
+        level: 40,
+        link: ""
+    },
+    {
+        skill: "Fortran",
+        description: "",
+        category: "Programming Language",
+        started: 2016,
+        level: 40,
+        link: "https://www.fortran.com"
+    },
+    {
+        skill: "GeoMedia",
+        description: "",
+        category: "GIS",
+        started: 2010,
+        level: 30,
+        link: ""
+    },
+    {
+        skill: "ArcGIS",
+        description: "",
+        category: "GIS",
+        started: 2013,
+        level: 90,
+        link: "http://desktop.arcgis.com"
+    },
+    {
+        skill: "QGIS",
+        description: "",
+        category: "GIS",
+        started: 2014,
+        level: 85,
+        link: "http://qgis.org"
+    },
+    {
+        skill: "FME Desktop",
+        description: "",
+        category: "GIS",
+        started: 2016,
+        level: 40,
+        link: "https://www.safe.com/fme/fme-desktop"
+    },
+    {
+        skill: "AutoCAD",
+        description: "",
+        category: "CAD",
+        started: 2013,
+        level: 50,
+        link: "https://www.autodesk.de/products/autocad/overview"
+    },
+    {
+        skill: "Revit",
+        description: "",
+        category: "BIM",
+        started: 2013,
+        level: 25,
+        link: "https://www.autodesk.de/products/revit/overview"
+    },
+    {
         skill: "CloudCompare",
         description: "3D point cloud and mesh processing software",
         category: "3D Software",
@@ -32,11 +88,35 @@ const skillsData = [
         link: "https://www.danielgm.net/cc/"
     },
     {
-        skill: "ESRI ArcGIS",
+        skill: "HTML",
         description: "",
-        category: "GIS",
-        started: 2011,
-        level: 90,
+        category: "Webdevelopment",
+        started: 2015,
+        level: 50,
+        link: ""
+    },
+    {
+        skill: "CSS",
+        description: "",
+        category: "Webdevelopment",
+        started: 2015,
+        level: 40,
+        link: ""
+    },
+    {
+        skill: "JavaScript",
+        description: "",
+        category: "Webdevelopment",
+        started: 2015,
+        level: 40,
+        link: ""
+    },
+    {
+        skill: "SQL",
+        description: "",
+        category: "Databases",
+        started: 2013,
+        level: 60,
         link: ""
     },
 ];
