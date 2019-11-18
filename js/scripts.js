442503a563852946f80706ab9bf38cf5689b71df
Tobias Rosskopf (privat) <tobirosskopf@gmail.com>
2019-11-18 11:30:16 +0100

    Added skillbars

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,33 @@
 function showEvents() {
-    var data_row =  "";
+    var table_rows =  "";
     for (let i = 0; i < eventsData.length; i++) {
         const event = eventsData[i];
-        data_row += "<tr><td>" + event.date + "</td><td><a href='" + event.link + "'>" + event.event + "</a></td><td>" + event.location + "</td></tr>";
+        table_rows += "<tr>" 
+            + "<td>" + event.date + "</td>"
+            + "<td><a href='" + event.link + "'>" + event.event + "</a></td>"
+            + "<td>" + event.location + "</td>"
+            + "</tr>";
     };
-    document.getElementById("events-data").innerHTML = data_row;
+    document.getElementById("events-data").innerHTML = table_rows;
 }
 
 
 function showSkills() {
-var data_row =  "";
+var table_rows =  "";
     for (let i = 0; i < skillsData.length; i++) {
         const skill = skillsData[i];
-        data_row += "<tr><td><a href='" + skill.link + "'>" + skill.skill + "</a></td><td>" + skill.category + "</td><td>" + skill.started + "</td><td>" + skill.level + " %</td></tr>";
-    };
-    document.getElementById("skills-data").innerHTML = data_row;
+        table_rows += "<tr>"
+            + "<td><a href='" + skill.link + "'>" + skill.skill + "</a></td>"
+            //+ "<td>" + skill.description + "</td>"
+            //+ "<td>" + skill.category + "</td>"
+            + "<td>" + skill.started + "</td>"
+            + "<td><div class='skillcontainer'><div class='skillbar' id='" + skill.skill + "'>" + skill.level + " %</div></div></td>"
+            + "</tr>";
+        };
+    document.getElementById("skills-data").innerHTML = table_rows;
+
+    for (let i = 0; i < skillsData.length; i++) {
+        const skill = skillsData[i];
+        document.getElementById(skill.skill).style.width = skill.level + "%";
+    }
 }
--- js/skillsData.js
+++ js/skillsData.js
@@ -1,23 +1,42 @@
 const skillsData = [
     {
         skill: "Python",
+        description: "",
+        category: "Programming Language",
         started: 2017,
         level: 80,
-        category: "Programming Language",
         link: "https://www.python.org"
     },
    {
         skill: "Fortran",
+        description: "",
+        category: "Programming Language",
         started: 2016,
         level: 30,
-        category: "Programming Language",
         link: "https://www.fortran.com"
     },
    {
         skill: "Java",
+        description: "",
+        category: "Programming Language",
         started: 2009,
         level: 65,
-        category: "Programming Language",
         link: "https://www.java.com"
+    },
+    {
+        skill: "CloudCompare",
+        description: "3D point cloud and mesh processing software",
+        category: "3D Software",
+        started: 2019,
+        level: 50,
+        link: "https://www.danielgm.net/cc/"
+    },
+    {
+        skill: "ESRI ArcGIS",
+        description: "",
+        category: "GIS",
+        started: 2011,
+        level: 90,
+        link: ""
     },
 ];
